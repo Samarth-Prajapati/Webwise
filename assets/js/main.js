a39f73aa480c99d85c043c9e1dac1f3b5acc33d4
Samarth Prajapati <samarthp475@gmail.com>
2024-03-24 20:01:32 +0530

    updated

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,24 +4,6 @@ if (preloader) {
     preloader.classList.add('d-none');
   }, 3000);
 }
-// const wrapper = document.querySelector('.wrapper');
-// const loginLink = document.querySelector('.login-link');
-// const registerLink = document.querySelector('.register-link');
-// const btnPopup = document.querySelector('.btnLogin-popup');
-// const iconClose = document.querySelector('.icon-close');
-// registerLink.addEventListener('click', () => {
-//   wrapper.classList.add('active');
-// });
-// loginLink.addEventListener('click', () => {
-//   wrapper.classList.remove('active');
-// });
-// btnPopup.addEventListener('click', () => {
-//   wrapper.classList.add('active-popup');
-// });
-// iconClose.addEventListener('click', () => {
-//   wrapper.classList.remove('active-popup');
-// });
-
 (function () {
   "use strict";
   const select = (el, all = false) => {
